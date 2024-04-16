43bab36927565fd1cb2a8e679bd8eedef3ab54b2
ayshakanta <ayshahussain291@gmail.com>
2024-04-17 04:35:57 +0600

    added navbar

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 const Root = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   observe auth state change
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    signInUser,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
